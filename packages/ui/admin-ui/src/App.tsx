49e70ca8235109b87c6a1251246b1c42c3ec9f41
VovaParamonov <vovaparamonov2000@mail.ru>
2022-03-15 17:44:29 +0600

    - Full implementation

--- packages/ui/admin-ui/src/App.tsx
+++ packages/ui/admin-ui/src/App.tsx
@@ -2,36 +2,19 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Map from "./Components/Map/Map";
 import { Amplify } from "aws-amplify";
-
-console.log('Env test: ', process.env.REACT_APP_IDENTITY_POOL_ID);
-
-Amplify.configure({
-  Auth: {
-    identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
-    region: "us-east-1",
-  },
-  geo: {
-    AmazonLocationService: {
-      maps: {
-        items: {
-          "TravelingSalesmansTaskDemo": {
-            style: "VectorEsriDarkGrayCanvas"
-          },
-        },
-        default: "TravelingSalesmansTaskDemo",
-      },
-      region: "us-east-1",
-    },
-  }
-});
+import GoogleLoginComp from "./Components/GoogleLogin/GoogleLogin";
+import { LoginProvider } from "./api/LoginContext";
 
 function App() {
   // const { categories } = useCategories();
 
   return (
-    <div className="App">
+    <LoginProvider>
+      <div className="App">
         <Map />
-    </div>
+        <GoogleLoginComp />
+      </div>
+    </LoginProvider>
   );
 }
 
--- packages/ui/admin-ui/src/api/routeCalc.ts
+++ packages/ui/admin-ui/src/api/routeCalc.ts
@@ -1,27 +1,68 @@
 import AWS  from 'aws-sdk';
 
-AWS.config.region = 'us-east-1';
-
-const credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID!
-});
-
-console.log("Credentials: ", credentials);
-
-const locationClient = new AWS.Location({
-  credentials: credentials,
-  region: 'us-east-1',
-});
-
 const paramsExample = {
   CalculatorName: "TravelingSalesmansTaskDemoRouteCalculator",
-  DeparturePosition: [-123.4567, 45.6789],
-  DestinationPosition: [-123.123, 45.234]
+  DeparturePosition: [73.36395111083993, 54.991179338182064],
+  DestinationPosition: [73.38043060302715, 54.98172389802377],
+  WaypointPositions: [
+    [73.3864387512213, 54.98428496633704]
+  ],
+  IncludeLegGeometry: true
 };
 
-export const testCalc = () => {
-  locationClient.calculateRoute(paramsExample, function(err, data) {
-    if (err) console.log('Error while calculation', err, err.stack); // an error occurred
-    else     console.log('Successful calculation: ', data);           // successful response
+export const buildRoute = (locationClient: AWS.Location | null) => async (coords: [number, number][]) => {
+  if (!locationClient) {
+    console.error('There is no locationCLient');
+    return;
+  }
+
+  const params = {
+    CalculatorName: "TravelingSalesmansTaskDemoRouteCalculator",
+    DeparturePosition: null as [number, number] | null, // fill it with coords
+    DestinationPosition: null as [number, number] | null, // fill it with coords
+    WaypointPositions: [] as [number, number][], // fill it with points
+    IncludeLegGeometry: true
+  }
+
+  params.DeparturePosition = coords[0];
+  params.DestinationPosition = coords[coords.length - 1];
+
+  params.WaypointPositions = coords.slice(1, coords.length - 1);
+
+  console.log('Used location: ', locationClient);
+
+  try {
+    return await locationClient.calculateRoute(params as any).promise();
+  } catch (e) {
+    console.error('Error when request route building');
+  }
+}
+
+export const getDistanceMatrix = (locationClient: AWS.Location | null) => async (coords: [number, number][]) => {
+  if (!locationClient) {
+    console.error('There is no locationCLient');
+    return;
+  }
+
+  const params = {
+    CalculatorName: "TravelingSalesmansTaskDemoRouteCalculator",
+    DeparturePositions: ([] as [number, number][]),
+    DestinationPositions: ([] as [number, number][]),
+    TravelMode: 'Car',
+  };
+
+  coords.forEach(coordsPair => {
+    params.DestinationPositions.push(coordsPair);
+    params.DeparturePositions.push(coordsPair)
   });
+
+  try {
+    const matrix = await locationClient.calculateRouteMatrix(params).promise();
+
+    console.log('Got calculatedMatrix: ', matrix);
+
+    return matrix;
+  } catch (e) {
+    console.error('Something went wrong with matrix calc: ', e);
+  }
 }
