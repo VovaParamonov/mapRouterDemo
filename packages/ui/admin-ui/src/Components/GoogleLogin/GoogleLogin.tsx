81572c4d8f9bd48fb758a785123f8794d379cd37
VovaParamonov <vovaparamonov2000@mail.ru>
2022-05-13 04:09:04 +0600

    - Env var separately taken out -

--- packages/ui/admin-ui/src/Components/GoogleLogin/GoogleLogin.tsx
+++ packages/ui/admin-ui/src/Components/GoogleLogin/GoogleLogin.tsx
@@ -1,34 +1,32 @@
 import React, { FunctionComponent, useContext } from 'react';
 import GoogleLogin from 'react-google-login';
 import './GoogleLogin.css';
-import { LoginContext } from "../../api/LoginContext";
+import { LoginContext } from '../../api/LoginContext';
 
-type GoogleLoginProps = {
-
-}
+type GoogleLoginProps = {}
 
 const errorGoogle = (response: any) => {
   console.error('Google login wrong result: ', response);
 }
 
 const GoogleLoginComp: FunctionComponent<GoogleLoginProps> = (props) => {
-  const { googleLoginStatement, setLoginStatement } = useContext(LoginContext);
+  const {googleLoginStatement, setLoginStatement} = useContext(LoginContext);
 
   const handleSuccessLogin = (loginData: any) => {
     setLoginStatement(loginData);
   }
 
-  return <div className='google_login' >
-    { !googleLoginStatement && (
+  return <div className='google_login'>
+    {!googleLoginStatement && (
       <GoogleLogin
         // autoLoad={true}
-        clientId="266832583436-f0mfim62si3g2isq9ohhsjm29vgo3anc.apps.googleusercontent.com"
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID!}
         buttonText="Войти"
         onSuccess={handleSuccessLogin}
         onFailure={errorGoogle}
         cookiePolicy={'single_host_origin'}
       />
-    ) }
+    )}
   </div>;
 }
 
--- packages/ui/admin-ui/src/Components/Map/Map.tsx
+++ packages/ui/admin-ui/src/Components/Map/Map.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
 
-import { createMap, createAmplifyGeocoder  } from "maplibre-gl-js-amplify";
+import { createMap, createAmplifyGeocoder  } from 'maplibre-gl-js-amplify';
 import {
   Marker,
   Map,
@@ -8,13 +8,13 @@ import {
   AnyPaint
 } from 'maplibre-gl';
 
-import useRouteCalculation from "../../hooks/useRouteCalculation";
-import MapControls from "./MapControls/MapControls";
+import useRouteCalculation from '../../hooks/useRouteCalculation';
+import MapControls from './MapControls/MapControls';
 
-import "maplibre-gl/dist/maplibre-gl.css";
-import "maplibre-gl-js-amplify/dist/public/amplify-map.css";
-import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
-import "maplibre-gl-js-amplify/dist/public/amplify-geocoder.css"; // Optional CSS for Amplify recommended styling
+import 'maplibre-gl/dist/maplibre-gl.css';
+import 'maplibre-gl-js-amplify/dist/public/amplify-map.css';
+import '@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css';
+import 'maplibre-gl-js-amplify/dist/public/amplify-geocoder.css'; // Optional CSS for Amplify recommended styling
 
 import './Map.css';
 
@@ -186,75 +186,19 @@ const MapComponent: FunctionComponent<MapProps> = (props) => {
   useEffect(() => {
     let tempMap: any;
     async function initializeMap() {
-      // We only want to initialize the underlying maplibre map after the div has been rendered
       if (mapRef.current != null) {
         tempMap = await createMap({
           container: mapRef.current,
           center: [73.4, 54.97],
-          // center: [-122.486052, 37.830348],
           zoom: 12,
         });
 
         // TODO: Configure geo https://docs.amplify.aws/cli/geo/search/
         tempMap.addControl(createAmplifyGeocoder());
 
-        tempMap.addControl(new NavigationControl(), "bottom-left");
+        tempMap.addControl(new NavigationControl(), 'bottom-left');
 
         setMap(tempMap);
-
-        // Route drawing
-        // tempMap.on('load', function () {
-        //   // tempMap.addSource('route', {
-        //   //   'type': 'geojson',
-        //   //   'data': {
-        //   //     'type': 'Feature',
-        //   //     'properties': {},
-        //   //     'geometry': {
-        //   //       'type': 'LineString',
-        //   //       'coordinates': [
-        //   //         [73.36390023034562, 54.99128730834794],
-        //   //         [73.36390022412039, 54.991287308742415],
-        //   //         [73.36365001335949, 54.99116999255551],
-        //   //         [73.3638600141169, 54.99102999205058],
-        //   //         [73.36435001588414, 54.99106999707529],
-        //   //         [73.36476000760197, 54.99118000235247],
-        //   //         [73.36476000465947, 54.9911800029038],
-        //   //         [73.36526999798292, 54.99130000937294],
-        //   //         [73.36635999979362, 54.9912499838972],
-        //   //         [73.36709001636729, 54.99122999420914],
-        //   //         [73.36831000240593, 54.991210004521086],
-        //   //         [73.36978999107093, 54.99116999097457],
-        //   //         [73.37001999207999, 54.99116999097457],
-        //   //         [73.37204000985611, 54.991109987739996],
-        //   //         [73.37273999064209, 54.99108999805195],
-        //   //         [73.37301998295648, 54.99107000836389],
-        //   //         [73.3735900136849, 54.99078000412027],
-        //   //         [73.37370998598364, 54.99065999765113],
-        //   //         [73.37416001024292, 54.99010001302234],
-        //   //         [73.37448999386265, 54.98966000069229],
-        //   //         [73.37479001003551, 54.98927999159682],
-        //   //         [73.37491001650464, 54.98912998351039],
-        //   //         [73.37557999567335, 54.98830999625527],
-        //   //         [73.37597999445687, 54.987819992619876],
-        //   //         [73.37653000132684, 54.98714000152195],
-        //   //         [73.37763999282558, 54.985660012856954],
-        //   //         [73.37809001708486, 54.98509001629894],
-        //   //         [73.37851000555645, 54.98463999203966],
-        //   //         [73.3787799859416, 54.9843700116545],
-        //   //         [73.379269989577, 54.983889985777935],
-        //   //         [73.38014000230787, 54.98308001045204],
-        //   //         [73.38027999846507, 54.98293000236562],
-        //   //         [73.38038999300498, 54.98274001490308],
-        //   //         [73.38038999595183, 54.98274000546658],
-        //   //         [73.38042994117937, 54.98172398203141]
-        //   //       ]
-        //   //     }
-        //   //   }
-        //   // });
-        //
-        // });
-
-
       }
     }
     initializeMap();
@@ -266,9 +210,9 @@ const MapComponent: FunctionComponent<MapProps> = (props) => {
   }, []);
 
 
-  return <div className="map_wrapper">
+  return <div className='map_wrapper'>
     <div className={`map__loading_bar ${loading ? 'active' : ''}`} />
-    <div ref={mapRef} id="map" className="fullheight-map" />
+    <div ref={mapRef} id='map' className='fullheight-map' />
     <MapControls
       clearMap={clearMap}
       markerCounter={markerCounter}
